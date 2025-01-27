cd9e1c49529fe5488debb95f4c6edc0b3310b93c
Stanislaw <stanislaw@janowicz.pl>
2025-01-27 23:24:17 +0100

    deleting obsolete

--- CarRenter/CarRenter.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using Blazored.LocalStorage;
-using CarRenter.Client.Classes;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddSingleton<CarService>();
-builder.Services.AddBlazoredLocalStorage();
-await builder.Build().RunAsync();
